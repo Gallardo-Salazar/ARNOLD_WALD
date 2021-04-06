7d4744a0abd7c557190e15d38644e102a15c64c5
unknown <pe_gasa@hotmail.com>
2021-04-06 16:28:23 +0200

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 69813194,
+    "boundingBox": {
+        "lx": 464690.2589111328,
+        "ly": 5163002.521427155,
+        "lz": 433.9989929199219,
+        "ux": 464927.8145866394,
+        "uy": 5163240.077102661,
+        "uz": 671.5546684265137
+    },
+    "tightBoundingBox": {
+        "lx": 464690.2587,
+        "ly": 5163002.5215,
+        "lz": 433.9991,
+        "ux": 464896.074,
+        "uy": 5163240.077,
+        "uz": 504.96990000000008
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 2.0572924613952638,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
